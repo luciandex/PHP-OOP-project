20500441ec458bc66487f4704330c1c86dfac50a
LucianDex <remindfwd@gmail.com>
2020-06-25 22:33:10 +0300

    Add files via upload

--- src/views/template/article.php
+++ src/views/template/article.php
@@ -12,20 +12,15 @@
                     <div class="item-holder">
                         <div class="image-box">
                             <figure>
-                                <a href="#"><img src="<?php echo RESOURCES_PATH; ?>images/blog/5.jpg<?php //$article->getImage(); ?>" alt=""></a>
+                                <a href="<?php echo $article->getUrl(); ?>"><img src="<?php echo RESOURCES_PATH; ?>images/blog/<?php echo $article->getImage(); ?>" alt=""></a>
                             </figure>
                         </div>
                         <div class="content-text">
-                            <a href="#"><h5>A LESSON IN SURGERY PREPAREDNESS - FROM HURRICANE SEASON<?php //$article->getTitle(); ?></h5></a>
-                            <span>By Donult Trump <?php //$article->getAuthor(); ?>/ 02 January 2018<?php //$article->getCreatedAt(); ?></span>
-                            <p>Interfaces Holisticly predominate elitslate extensible testing procedures reliable tell and supply chains engage. top-line web services.Dramatically consectetur adi pisi cing elit, sed do eiusmod tempor lines exercitationemut labore love life’s sweetest reward Let it flow it floats back to you. <br>
-                            Enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magniol res eos qui rate voluptatem sequi nesciunt Neque porro quisquam est qui dolorem ipsum quia dolore sit amet conlites sectetur adipisci quia suthagara lukuthea satham non numquam eius modi tempra. incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam.
-                                <?php //$article->getText(); ?>
-                            </p>
+                            <a href="<?php echo $article->getUrl(); ?>"><h5><?php echo $article->getTitle(); ?></h5></a>
+                            <span>By <?php echo $article->getAuthor(); ?> / <?php echo $article->getCreatedAt(); ?></span>
+                            <p><?php echo $article->getText(); ?></p>
                             <div class="text">
-                                <p>
-                                    Incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosamincidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam.
-                                </p>
+<!--                                <p>--><?php //echo $article->getText(); ?><!--</p>-->
                             </div>                            
                         </div>
                     </div>
