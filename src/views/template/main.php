c9dec6c868f2552991c43a219aa0cb56bcd0d01f
LucianDex <remindfwd@gmail.com>
2020-06-25 22:34:02 +0300

    Add files via upload

--- src/views/template/main.php
+++ src/views/template/main.php
@@ -15,9 +15,8 @@ $footerMenuItems = (new MenuModel())
     ->getMenu(MenuModel::FOOTER_LINKS)
     ->getMenuItems();
 
-
 $recentArticles = new ArticleModel();
-for ($i = 1; $i < 28; $i++) {
+for ($i = 1; $i < 10; $i++) {                          // correct form will be $i < count($articles) or something similar
     $footerArticles[] = $recentArticles->getArticle($i);
 }
 
@@ -28,19 +27,14 @@ include_once VIEWS_PATH . 'template/main-menu.php';
 
 include_once VIEWS_PATH . 'template/page-title.php';
 
-//var_dump($viewFile);
 
 try {
     if (file_exists($viewFile)) {
         include_once $viewFile;
     }
-
-} catch (\RuntimeException $e1) {
-    error_log($e1->getMessage());
-    echo("I was unable to find that page!");
-} catch (\InvalidArgumentException $e2) {
-    error_log($e2->getMessage());
-    echo("Unable to get that page!");
+} catch (\InvalidArgumentException $invalidArgumentException) {
+    error_log($invalidArgumentException->getMessage());
+    echo("Not possible to get that page!");
 }
 
 
