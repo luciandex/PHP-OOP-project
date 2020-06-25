a8e24c9fa1db78ec002b194e09a251f44da63d63
LucianDex <remindfwd@gmail.com>
2020-06-25 19:52:00 +0300

    Add files via upload

--- src/Application.php
+++ src/Application.php
@@ -4,15 +4,16 @@
 namespace App;
 
 use App\controllers\DefaultController;
+use http\Exception\InvalidArgumentException;
 
 class Application
 {
     private const DEFAULT_CONTROLLER = DefaultController::class;
     private const DEFAULT_ACTION = "index";
 
-    private $config;
     private static $instance = null;
 
+    private $config;
     private $controller = null;
     private $action = null;
     private $params = [];
@@ -26,50 +27,11 @@ class Application
     }
 
     /**
-     * Create new controller instance
-     *
-     * @param mixed $controller
-     */
-    public function setController(?string $controller)
-    {
-
-        if ($controller === null) {
-            $this->controller = self::DEFAULT_CONTROLLER;
-            return $this;
-        }
-
-        //UserController
-        $controller = ucfirst(strtolower($controller) . "Controller"); //  . "Controller"
-        $fullControllerName = "App\\controllers\\" . $controller;
-
-        // validate that class exists
-        if (class_exists($fullControllerName) === false) {
-            throw new \InvalidArgumentException("Controller { $fullControllerName } not found");
-        }
-        $this->controller = $fullControllerName;
-        return $this;
-    }
-
-    /**
      * @return mixed
      */
     public function getAction()
     {
         return $this->action;
-    }
-
-    /**
-     * Set action from controller
-     */
-    public function setAction(?string $action)
-    {
-        if ($action === null) {
-            $this->action = self::DEFAULT_ACTION;
-            return $this;
-        }
-        // validate that action exists in $this->controller | ReflectionClass -> hasMethod
-        $this->action = strtolower($action);
-        return $this;
     }
 
     /**
@@ -90,6 +52,10 @@ class Application
         return $this;
     }
 
+    /**
+     * @param array $config
+     * @return Application|null
+     */
     public static function getInstance(array $config = [])
     {
         if (self::$instance === null) {
@@ -98,45 +64,98 @@ class Application
         return self::$instance;
     }
 
-    private function __construct(array $config = [])
+
+    /**
+     *** Create new controller instance ***
+     * @param mixed $controller
+     */
+    public function setController(?string $controller)
     {
-        $this->config = $config;
+        if ($controller === null) {
+            $this->controller = self::DEFAULT_CONTROLLER;
+            return $this;
+        }
+
+        //SetController
+        $controller = ucfirst(strtolower($controller) . "Controller");
+        $fullControllerName = "App\\controllers\\" . $controller;
+
+        if (class_exists($fullControllerName) === false) {
+            (new DefaultController())->errorPage404();
+            throw new InvalidArgumentException("Page $this->controller not found");
+        }
+
+        $this->controller = $fullControllerName;
+        return $this;
+
+    }
+
+    /**
+     * Set action from controller
+     */
+    public function setAction(?string $action)
+    {
+        if ($action === null) {
+            $this->action = self::DEFAULT_ACTION;
+            return $this;
+        }
+
+        $checkAction = new \ReflectionClass($this->controller);
+
+        if ($checkAction->hasMethod($this->action)) {
+            $this->action = strtolower($action);
+            throw new InvalidArgumentException("That move can not be processed");
+        }
+
+        return $this;
     }
 
 
     private function parseUri()
     {
         $path = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_QUERY), "/");
-
         $data = [];
-        parse_str($path, $data); // $data is passed by reference and modified inside parse_str function
+        parse_str($path, $data);
 
         $this->setController($data['controller'] ?? null);
         unset($data['controller']);
+
         $this->setAction($data['action'] ?? null);
         unset($data['action']);
-        $this->setParams($data ?? []);
 
+        $this->setParams($data ?? []);
     }
 
     public function run()
     {
         $this->parseUri();
         $result = call_user_func_array([new $this->controller, $this->action], $this->params);
-//var_dump($result);
 
-
-        if ($result === false || $result === null) {
-
-@            header("Location: " . VIEWS_PATH . "default/404.php");
-//            throw new \RuntimeException("Something went wrong");
+        if ($result === false || $this->controller === null) {
+            (new DefaultController())->errorPage404();
+            throw new \RuntimeException("$this->controller was not found or not possible to initiate");
         }
     }
 
+    /**
+     * Application constructor.
+     * @param array $config
+     */
+    private function __construct(array $config = [])
+    {
+        $this->config = $config;
+    }
+
+    /**
+     * Application clone
+     */
     private function __clone()
     {
     }
 
+    /**
+     * Application wakeup
+     */
     private function __wakeup()
     {
     }
