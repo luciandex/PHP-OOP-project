790d1ac7073d9c59b97720bf4d850a1953c9bda0
LucianDex <remindfwd@gmail.com>
2020-07-01 00:17:56 +0300

    Add files via upload

--- src/views/template/page-title.php
+++ src/views/template/page-title.php
@@ -1,31 +1,57 @@
 <?php
 
+if (isset($_GET['controller'])) {
+    if (isset($_GET['slug'])) {
+        if ($_GET['slug'] == 'home') {
+            echo '';
+        } else {
+            ?>
 
-// get page title section only if page slug exist
-// (for home page "page slug" does not exist; it will be shown slider by default)
+<!--          Page Title-->
+            <section class="page-title text-center"
+                     style="background-image:url(<?php echo RESOURCES_PATH; ?>images/background/3.jpg);">
+                <div class="container">
+                  <div class="title-text">
+                      <h1><?php echo $page->getTitle(); ?>
+                        </h1>
+                        <ul class="title-menu clearfix">
+                            <li>
+                                <a href="<?php echo BASE_URL; ?>">Home &nbsp;/</a>
+                            </li>
+                            <li><?php echo $page->getTitle(); ?>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </section>
+<!--            End Page Title-->
 
-if (!isset($_GET['controller'])) {
-    echo '';
-} elseif($_GET['controller'] != 'default') {
+            <?php
+        }
+    } elseif (!isset($_GET['slug'])) {
+        ?>
 
-?>
+<!--       Page Title-->
+        <section class="page-title text-center"
+                 style="background-image:url(<?php echo RESOURCES_PATH; ?>images/background/3.jpg);">
+           <div class="container">
+               <div class="title-text">
+                   <h1><?php echo $page->getTitle(); ?>
+                    </h1>
+                    <ul class="title-menu clearfix">
+                        <li>
+                            <a href="<?php echo BASE_URL; ?>">Home &nbsp;/</a>
+                        </li>
+                        <li><?php echo $page->getTitle(); ?>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+<!--     End Page Title-->
 
-<!--Page Title-->
-<section class="page-title text-center" style="background-image:url(<?php echo RESOURCES_PATH; ?>images/background/3.jpg);">
-    <div class="container">
-        <div class="title-text">
-            <h1>
-                <?php echo $page->getTitle();?>
-            </h1>
-            <ul class="title-menu clearfix">
-                <li>
-                    <a href="<?php echo BASE_URL; ?>">Home &nbsp;/</a>
-                </li>
-                <li><?php echo $page->getTitle(); ?></li>
-            </ul>
-        </div>
-    </div>
-</section>
-<!--End Page Title-->
+        <?php
+    }
+}
 
-<?php  }; // end brace for ELSEIF statement
+
