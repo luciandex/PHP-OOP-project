17915f40b3d5f3f022b1c67ca8d240456b1599a9
LucianDex <remindfwd@gmail.com>
2020-07-06 20:41:49 +0300

    Add files via upload

--- src/views/template/page-title.php
+++ src/views/template/page-title.php
@@ -1,42 +1,33 @@
 <?php
 
-if (isset($_GET['controller'])) {
-    if (isset($_GET['slug'])) {
-        if ($_GET['slug'] == 'home') {
+if (isset($_GET['controller']))
+{
+
+    if (isset($_GET['slug']))
+    {
+
+        if ($_GET['slug'] == 'home')
+        {
             echo '';
-        } else {
-            ?>
-
-<!--          Page Title-->
-            <section class="page-title text-center"
-                     style="background-image:url(<?php echo RESOURCES_PATH; ?>images/background/3.jpg);">
-                <div class="container">
-                  <div class="title-text">
-                      <h1><?php echo $page->getTitle(); ?>
-                        </h1>
-                        <ul class="title-menu clearfix">
-                            <li>
-                                <a href="<?php echo BASE_URL; ?>">Home &nbsp;/</a>
-                            </li>
-                            <li><?php echo $page->getTitle(); ?>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </section>
-<!--            End Page Title-->
-
-            <?php
         }
-    } elseif (!isset($_GET['slug'])) {
+        else
+        {
+            goto titleSection;
+        }
+
+    }
+    else {
+
+        titleSection:
+
         ?>
 
-<!--       Page Title-->
+        <!--          Page Title-->
         <section class="page-title text-center"
                  style="background-image:url(<?php echo RESOURCES_PATH; ?>images/background/3.jpg);">
-           <div class="container">
-               <div class="title-text">
-                   <h1><?php echo $page->getTitle(); ?>
+            <div class="container">
+                <div class="title-text">
+                    <h1><?php echo $page->getTitle(); ?>
                     </h1>
                     <ul class="title-menu clearfix">
                         <li>
@@ -48,10 +39,10 @@ if (isset($_GET['controller'])) {
                 </div>
             </div>
         </section>
-<!--     End Page Title-->
+        <!--            End Page Title-->
 
         <?php
+
     }
+
 }
-
-
