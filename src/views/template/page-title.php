a75436e0b1ad5166a0a2560cac5a512c519efa0a
LucianDex <remindfwd@gmail.com>
2020-09-01 22:35:20 +0300

    Add files via upload

--- src/views/template/page-title.php
+++ src/views/template/page-title.php
@@ -1,48 +1,31 @@
 <?php
 
-if (isset($_GET['controller']))
-{
 
-    if (isset($_GET['slug']))
-    {
+// get page title section only if page slug exist
+// (for home page "page slug" does not exist; it will be shown slider by default)
 
-        if ($_GET['slug'] == 'home')
-        {
-            echo '';
-        }
-        else
-        {
-            goto titleSection;
-        }
+if (!isset($_GET['slug'])) {
+    echo '';
+} else {
+    $slug = $_GET['slug'];
+    ?>
 
-    }
-    else {
-
-        titleSection:
-
-        ?>
-
-        <!--          Page Title-->
-        <section class="page-title text-center"
-                 style="background-image:url(<?php echo RESOURCES_PATH; ?>images/background/3.jpg);">
-            <div class="container">
-                <div class="title-text">
-                    <h1><?php echo $page->getTitle(); ?>
-                    </h1>
-                    <ul class="title-menu clearfix">
-                        <li>
-                            <a href="<?php echo BASE_URL; ?>">Home &nbsp;/</a>
-                        </li>
-                        <li><?php echo $page->getTitle(); ?>
-                        </li>
-                    </ul>
-                </div>
+    <!--Page Title-->
+    <section class="page-title text-center" style="background-image:url(<?php echo RESOURCES_PATH; ?>images/background/3.jpg);">
+        <div class="container">
+            <div class="title-text">
+                <h1>
+                    <?php echo $page->getTitle();?>
+                </h1>
+                <ul class="title-menu clearfix">
+                    <li>
+                        <a href="<?php echo BASE_URL; ?>"><?php echo 'home'; ?> &nbsp;/</a>
+                    </li>
+                    <li><?php echo $page->getTitle(); ?></li>
+                </ul>
             </div>
-        </section>
-        <!--            End Page Title-->
+        </div>
+    </section>
+    <!--End Page Title-->
 
-        <?php
-
-    }
-
-}
+<?php  }; // end brace for ELSE statement
